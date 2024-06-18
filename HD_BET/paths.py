f9c11b79a0f9e76131a1b23f860c37de619ba1fa
Ray Davey <57007488+raymondfdavey@users.noreply.github.com>
2024-06-18 18:55:38 +0100

    Modified for working within the repo/docker world

--- HD_BET/paths.py
+++ HD_BET/paths.py
@@ -1,4 +1,4 @@
 import os
 
-# please refer to the readme on where to get the parameters. Save them in this folder:
-folder_with_parameter_files = os.path.join(os.path.expanduser('~'), 'hd-bet_params')
+# Update the folder path to the location where you have saved the parameter files
+folder_with_parameter_files = "../hd-bet_params"
