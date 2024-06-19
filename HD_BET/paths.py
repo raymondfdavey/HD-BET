eddd2c1b8db69c5e955117fa50f6382209c67f18
Ray Davey <57007488+raymondfdavey@users.noreply.github.com>
2024-06-19 08:42:12 +0100

    model params added

--- HD_BET/paths.py
+++ HD_BET/paths.py
@@ -1,4 +1,4 @@
 import os
 
 # Update the folder path to the location where you have saved the parameter files
-folder_with_parameter_files = "../hd-bet_params"
+folder_with_parameter_files = "./hd-bet_params"
